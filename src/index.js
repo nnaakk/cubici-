4ec2e30503c7a3807c251c241db484778a237d0f
nnaakk <102175783+nnaakk@users.noreply.github.com>
2023-11-21 20:29:54 +0200

    set config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Initial');
+const express = require ('express')
+
+const config = require*('./config')
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('Home Page')
+})
+
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`))
