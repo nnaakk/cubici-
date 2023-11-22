3532aa2075ff8f781635f014ba48a8adec6c555a
nnaakk <102175783+nnaakk@users.noreply.github.com>
2023-11-22 18:25:31 +0200

    end express configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require ('express')
 
+
 const config = require('./config')
 
 const app = express()
 
+const setupViewEngine = require('./config/viewEngine')
+
+setupViewEngine(app)
+
 app.get('/', (req,res) => {
     res.send('Home Page')
+})
+
+app.get('/hbs' , (req,res) => {
+    res.render('home' )
 })
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`))
