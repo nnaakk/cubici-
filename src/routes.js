57b4969618868d1a09b97208bc732b96f84503b0
nnaakk <102175783+nnaakk@users.noreply.github.com>
2023-11-28 20:36:32 +0200

    get create

--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,21 @@ const router = require('express').Router()
 const  cubeController  = require('./controllers/cubeController')
 const  homeController  = require('./controllers/homeController')
 
-
+const accessoryController = require('./controllers/accessoryController')
 
 
 router.get('/' , homeController.getHome)
-
 router.get('/about' , homeController.getAbout)
-
-router.get('/create' , cubeController.getCreateCube)
-
-router.post('/create' , cubeController.postCreateCube)
-
-router.get('/details/:cubeId' , cubeController.getDetails)
-
 router.get('/404' , homeController.getError)
 
-router.get('*', homeController.getError)
+router.get('/create' , cubeController.getCreateCube)
+router.post('/create' , cubeController.postCreateCube)
+router.get('/details/:cubeId' , cubeController.getDetails)
+
+
+
+//router.get('*', homeController.getError)
+
+router.use('/accessory', accessoryController)
 
 module.exports = router
