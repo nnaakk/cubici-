402a508e217e63cea173f11cdfc812308db00175
nnaakk <102175783+nnaakk@users.noreply.github.com>
2023-11-28 23:04:21 +0200

    add accesoory cod

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,10 +1,22 @@
 const router = require('express').Router()
-
+const Accessory = require('../models/Accessory')
 
 
 
 router.get('/create',(req,res) => {
    res.render('createAccessory')
+})
+
+router.post('/create', async (req,res) => {
+const {name, description,imageUrl} = req.body
+
+let accessory = new Accessory({name, description,imageUrl})
+
+await accessory.save(accessory)
+
+res.redirect('/')
+
+
 })
 
 module.exports = router
